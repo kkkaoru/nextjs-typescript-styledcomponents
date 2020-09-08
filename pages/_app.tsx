ab6044d89ecbedcb3d92e68adac7fc06262fe308
kaoru.ohtaka <ockacru+git@gmail.com>
2020-09-09 03:57:06 +0900

    :fire: Remove almost css and use styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { AppProps } from 'next/app';
 import { BaseContext } from 'next/dist/next-server/lib/utils';
 import 'sanitize.css';
-import '../styles/globals.css';
+import '@/styles/global.css';
 
 export default function App({ Component, pageProps }: AppProps): BaseContext {
   return <Component {...pageProps} />;
