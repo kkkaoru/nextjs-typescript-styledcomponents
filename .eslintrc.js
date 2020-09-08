5f914451125756c513d4a7650c7360a36d7e707b
kaoru.ohtaka <ockacru+git@gmail.com>
2020-09-09 02:02:45 +0900

    :construction_worker: Can use eslint and prettier with typescript

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/** @type import('eslint').Linter.BaseConfig */
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: [
+    '@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    'project': './tsconfig.json',
+  },
+  env: {
+    'browser': true,
+    'node': true,
+    'es6': true,
+  },
+}
