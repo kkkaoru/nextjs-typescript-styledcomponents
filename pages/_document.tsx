9be152052ed120083ce8a44e5b46f77f2514c769
kaoru.ohtaka <ockacru+git@gmail.com>
2020-09-09 09:44:58 +0900

    :heavy_plus_sign: Add viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -34,6 +34,7 @@ export default class MyDocument extends Document<Props> {
       <Html lang="ja">
         <Head>
           <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
           {this.props.styleTags}
         </Head>
         <body>
