c266d0d09d4705ee2698273a68944362b50328e0
kaoru.ohtaka <ockacru+git@gmail.com>
2020-12-04 14:14:47 +0900

    :construction_worker: Add airbnb typescript eslint conf

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 /** @type import('eslint').Linter.BaseConfig */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-    'prettier/react',
-  ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
+  extends: ['airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   env: {
     browser: true,
     node: true,
     es6: true,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
   },
 };
