96fd4da237570449e4c9087b547832db411e35da
kaoru.ohtaka <ockacru+git@gmail.com>
2020-12-04 13:55:11 +0900

    :green_heart: Update prettier conf in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,8 +3,9 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
     'prettier/@typescript-eslint',
+    'prettier/react',
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
