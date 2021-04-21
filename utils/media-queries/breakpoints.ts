ff2dcb2c7dba8d03dbc8422d514badc65c9b7eae
kaoru.ohtaka <ockacru+git@gmail.com>
2021-04-21 18:48:36 +0900

    Update breakpoints based on bootstrap

--- utils/media-queries/breakpoints.ts
+++ utils/media-queries/breakpoints.ts
@@ -1,16 +1,18 @@
-// 374px is less than iPhone8, iPhone SE2,
-export const widthXS = 374;
-export const widthSm = 600;
-export const widthMd = 960;
-export const widthLg = 1280;
-export const widthXl = 1920;
+// Breakpoints are inspired bootstrap
+// https://getbootstrap.com/docs/5.0/layout/breakpoints/
+export const widthSm = 576;
+export const widthMd = 768;
+export const widthLg = 992;
+export const widthXl = 1200;
+export const widthXxl = 1400;
 
 function buildBreakpointsWithMinWidth(breakpoint: number): string {
   return `(min-width: ${breakpoint}px)`;
 }
 
-export const xs = `(max-width: ${widthXS}px)`;
+export const xs = `(max-width: ${widthSm - 1}px)`;
 export const sm = buildBreakpointsWithMinWidth(widthSm);
 export const md = buildBreakpointsWithMinWidth(widthMd);
 export const lg = buildBreakpointsWithMinWidth(widthLg);
 export const xl = buildBreakpointsWithMinWidth(widthXl);
+export const xxl = buildBreakpointsWithMinWidth(widthXxl);
